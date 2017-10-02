fe5f1405093b06041cee3ad4d4b869a49e4504c6
Aleksandr <shuto4ka1@gmail.com>
2017-10-02 05:04:12 +0300

    Migrated to hsqldb

--- src/main/java/testtask/dirsandfiles/config/BeanConfig.java
+++ src/main/java/testtask/dirsandfiles/config/BeanConfig.java
@@ -2,7 +2,10 @@ package testtask.dirsandfiles.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import javax.sql.DataSource;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.time.Clock;
@@ -26,5 +29,15 @@ public class BeanConfig {
     @Bean
     public FileSystem getFileSystem() {
         return FileSystems.getDefault();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("db/initDB_hsql.sql")
+                .addScript("db/populate_db.sql")
+                .build();
     }
 }
